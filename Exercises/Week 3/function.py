ca7477fa531da06c5622f83f1df87bbd34512f79
EricSimoKouche <eric.simokouche@student.uclouvain.be>
2026-02-22 02:47:58 +0100

    TP1 Week 3

---
+++ Exercises/Week 3/function.py
@@ -0,0 +1,12 @@
+def enum_Z() :
+    print(0)
+    n = 0
+    while True:
+        n+=1
+        print(n)
+        print(-n)
+
+def enum_abc ():
+    alphabet = {"a", "b", "c"}
+    print("")
+    list
